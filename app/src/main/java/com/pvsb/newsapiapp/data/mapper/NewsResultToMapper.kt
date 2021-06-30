67302ad7d194d223899f31e65df2b3298aa0e303
PAULO VICTOR SIMIAO BORGES <borgesgamepay@gmail.com>
2021-06-30 12:49:20 -0300

    Mapping the data from our our bodyResponse to our details response.

---
+++ app/src/main/java/com/pvsb/newsapiapp/data/mapper/NewsResultToMapper.kt
@@ -0,0 +1,18 @@
+package com.pvsb.newsapiapp.data.mapper
+
+import com.pvsb.newsapiapp.data.response.NewsBodyResponse
+import com.pvsb.newsapiapp.data.response.NewsDetailsResponse
+
+object NewsResultToMapper {
+
+    fun resultMap(source: NewsBodyResponse): List<NewsDetailsResponse>{
+        val newsResultList: MutableList<NewsDetailsResponse> = mutableListOf()
+        source.let {newsBodyResponse ->
+            for (newsResult in newsBodyResponse.newsDetails){
+                newsResultList.add(newsResult)
+            }
+        }
+        return newsResultList
+    }
+
+}
