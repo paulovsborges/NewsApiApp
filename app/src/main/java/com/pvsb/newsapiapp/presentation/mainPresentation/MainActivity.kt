7e54f9f45327a8d668cdecd2ea9525f1dc23fb8c
PAULO VICTOR SIMIAO BORGES <borgesgamepay@gmail.com>
2021-06-30 12:44:50 -0300

    Getting the data from our api to be displayed to the user.

---
+++ app/src/main/java/com/pvsb/newsapiapp/presentation/mainPresentation/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.pvsb.newsapiapp.presentation.mainPresentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Adapter
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.pvsb.newsapiapp.databinding.ActivityMainBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
+    private lateinit var mAdapter: MainAdapter
+
+    private val viewModel: MainViewModel by viewModel()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initiateAdapter()
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+
+        viewModel.newsLiveData.observe(this, Observer { listNews ->
+            mAdapter.getNews(listNews)
+        })
+        viewModel.getNewsViewModel()
+    }
+
+    private fun initiateAdapter() {
+        with(binding.recyclerViewMain){
+            layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+            mAdapter = MainAdapter(this@MainActivity)
+            adapter = mAdapter
+        }
+    }
+}
