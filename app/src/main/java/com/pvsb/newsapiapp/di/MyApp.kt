c21a2ce0057214c879745741de44ef5ed80ed2cc
PAULO VICTOR SIMIAO BORGES <borgesgamepay@gmail.com>
2021-06-30 12:47:11 -0300

    Implementing the core of the Koin.

---
+++ app/src/main/java/com/pvsb/newsapiapp/di/MyApp.kt
@@ -0,0 +1,30 @@
+package com.pvsb.newsapiapp.di
+
+import android.app.Application
+import com.pvsb.newsapiapp.di.modules.networkModule
+import com.pvsb.newsapiapp.di.modules.repositoryModule
+import com.pvsb.newsapiapp.di.modules.viewModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class MyApp:Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initiateKoin()
+    }
+
+    private fun initiateKoin() {
+        startKoin {
+            androidContext(this@MyApp)
+            androidLogger()
+            modules(
+                viewModule,
+                repositoryModule,
+                networkModule
+            )
+        }
+    }
+
+}
