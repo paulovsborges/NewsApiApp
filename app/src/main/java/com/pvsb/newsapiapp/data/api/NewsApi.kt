541d615087ac2a364c19bf33efb7d6274b33d70f
PAULO VICTOR SIMIAO BORGES <borgesgamepay@gmail.com>
2021-06-30 12:47:31 -0300

    Implementing the endPoint.

---
+++ app/src/main/java/com/pvsb/newsapiapp/data/api/NewsApi.kt
@@ -0,0 +1,15 @@
+package com.pvsb.newsapiapp.data.api
+
+import com.pvsb.newsapiapp.data.response.NewsBodyResponse
+import com.pvsb.newsapiapp.model.NewsEntity
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface NewsApi {
+
+    @GET("/v2/everything?q=bitcoin&apiKey=eb14adb4f30842efa48f919d726b52e8")
+    suspend fun getNews(
+     //   @Query("articles")sources : String = ""
+
+    ) : NewsBodyResponse
+}
