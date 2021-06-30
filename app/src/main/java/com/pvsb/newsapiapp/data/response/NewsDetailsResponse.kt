cc3731b99d5d5794a0e348e6b89e2750ec8ad9fb
PAULO VICTOR SIMIAO BORGES <borgesgamepay@gmail.com>
2021-06-30 12:48:21 -0300

    Implementing the details of the articles of the api.

---
+++ app/src/main/java/com/pvsb/newsapiapp/data/response/NewsDetailsResponse.kt
@@ -0,0 +1,37 @@
+package com.pvsb.newsapiapp.data.response
+
+import com.google.gson.annotations.SerializedName
+import com.pvsb.newsapiapp.model.NewsEntity
+
+class NewsDetailsResponse (
+
+    @SerializedName("author")
+    val author: String,
+
+    @SerializedName("title")
+    val title: String,
+
+    @SerializedName("description")
+    val description: String,
+
+    @SerializedName("url")
+    val url: String,
+
+    @SerializedName("urlToImage")
+    val urlToImage: String,
+
+    @SerializedName("publishedAt")
+    val publishedAt: String
+
+
+){
+
+    fun getNewsModel() =  NewsEntity(
+        author= this.author,
+        title = this.title,
+        description = this.description,
+        url = this.url,
+        urlToImage = this.urlToImage,
+        publishedAt = this.publishedAt,
+    )
+}
