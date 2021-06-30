533ba1c01aa6f1b0c59abb84c482c292060bae97
PAULO VICTOR SIMIAO BORGES <borgesgamepay@gmail.com>
2021-06-30 12:51:49 -0300

    Implementing the instance of the retrofit and creating the interceptor.

---
+++ app/src/main/java/com/pvsb/newsapiapp/data/retrofit/retrofit.kt
@@ -0,0 +1,35 @@
+package com.pvsb.newsapiapp.data.retrofit
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+private const val CONNECTION_TIMEOUT: Long = 6000L
+private const val BASEURL: String = "https://newsapi.org/"
+
+
+internal fun provideOkHttpClient(): OkHttpClient {
+
+    val client = OkHttpClient.Builder()
+        .connectTimeout(CONNECTION_TIMEOUT, TimeUnit.MILLISECONDS)
+        .readTimeout(CONNECTION_TIMEOUT, TimeUnit.MILLISECONDS)
+
+    val logging = HttpLoggingInterceptor()
+    logging.level = HttpLoggingInterceptor.Level.BODY
+    client.addInterceptor(logging)
+
+    return client.build()
+}
+
+internal fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+
+    return Retrofit.Builder()
+        .baseUrl(BASEURL)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+}
+
+internal inline fun <reified T> createApi(retrofit: Retrofit) = retrofit.create(T::class.java)
