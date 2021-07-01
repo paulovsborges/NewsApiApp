7eb7df4588d5b2ddbebe8b73250c4aa4adb7ed25
PAULO VICTOR SIMIAO BORGES <borgesgamepay@gmail.com>
2021-07-01 17:05:35 -0300

    Finishing to implement the item click and implementing the swipe refresh.

--- app/src/main/java/com/pvsb/newsapiapp/model/constants/AppConstants.kt
+++ app/src/main/java/com/pvsb/newsapiapp/model/constants/AppConstants.kt
@@ -1,2 +1,14 @@
 package com.pvsb.newsapiapp.model.constants
 
+
+object AppConstants {
+
+    const val INTENT_AUTHOR = "author"
+    const val INTENT_TITLE = "title"
+    const val INTENT_DESCRIPTION = "description"
+    const val INTENT_URL = "url"
+    const val INTENT_URL_TO_IMAGE = "urlToImage"
+    const val INTENT_PUBLISHED_AT = "publishedat"
+    const val INTENT_CONTENT = "content"
+
+}
